67f7dd54561035df684004f914d915b25bddad4c
Rafal Mitula (PGS Software) <rmitula@pgs-soft.com>
2019-04-23 11:35:47 +0200

    added partition by company name

--- source/nyc-taxi-data-step-function-worker/lambda_function.py
+++ source/nyc-taxi-data-step-function-worker/lambda_function.py
@@ -4,19 +4,19 @@ import boto3
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    
+
     sourceBucket = "nyc-tlc"
-    targetBucket = "rmitula-nyc-taxi-data-3"
+    targetBucket = "rmitula-nyc-taxi"
     prefix = "trip data/"
     index = event.get("iterator").get("index")
-    
+
     objects = s3.list_objects(Bucket=sourceBucket, Prefix=prefix)['Contents']
     sourceFile = objects[index]['Key']
     year = sourceFile.split('_')[2].split('-')[0]
     month = sourceFile.split('-')[1].split('.')[0]
-    targetFile = sourceFile.split('/')[1].split('_')[0]
-    targetFilePath = f"{year}/{month}/{targetFile}.csv"
-    
+    companyName = sourceFile.split('/')[1].split('_')[0]
+    targetFilePath = f"data/{companyName}/{year}/{month}/{companyName}.csv"
+
     message = f"Copy from {sourceFile} -> {targetFilePath}"
     print(message)
 
@@ -24,10 +24,10 @@ def lambda_handler(event, context):
         "index": event.get("iterator").get("index"),
         "step": event.get("iterator").get("step")
     }
-    
+
     copySource = {
-    'Bucket': sourceBucket,
-    'Key': sourceFile
+        'Bucket': sourceBucket,
+        'Key': sourceFile
     }
 
     s3.copy(copySource, targetBucket, targetFilePath)
